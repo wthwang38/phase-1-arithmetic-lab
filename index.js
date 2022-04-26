ba128990be79c575db44ce6a8f4cdf40dcf79de0
William Hwang <willhwang90@gmail.com>
2022-04-26 00:13:09 -0400

    commit tree 143a5e3e5a25957bc2079a649adae0f0eb790d57

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+const num3 = 12;
+const num4 = 3;
+const mod = num3 / num4;
+
+const max = Math.max(20, 19, 18, 5);
